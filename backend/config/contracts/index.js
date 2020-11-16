310b9b28f79d57e3ffc29cc55fd79c79dcf7ba0b
Akshay Meher <akshay_111meher@hotmail.com>
2020-11-16 12:51:45 +0530

    Sending Token Contract Details with API

--- backend/config/contracts/index.js
+++ backend/config/contracts/index.js
@@ -1,10 +1,13 @@
 const distributionAbi = require("./distributionAbi.json");
 const stakeRegistryAbi = require("./stakeRegistryAbi.json");
+const tokenAbi = require("./tokenAbi.json");
 
 const distributionAddress = process.env.DISTRIBUTION_CONTRACT_ADDRESS;
 const stakeRegistryAddress = process.env.STAKE_REGISTRY_CONTRACT_ADDRESS;
+const tokenAddress = process.env.TOKEN_CONTRACT_ADDRESS;
 
 module.exports = {
   distribution: { abi: distributionAbi, address: distributionAddress },
   stakeRegistry: { abi: stakeRegistryAbi, address: stakeRegistryAddress },
+  token: { abi: tokenAbi, address: tokenAddress },
 };
--- backend/config/init.js
+++ backend/config/init.js
@@ -43,6 +43,9 @@ async function init() {
       "15ictvkBL2D3aWxyoqh8roJkRC1tdFw3SCLqjyssjuf6yiC9",
       "14bARWgpfEiURUS7sGGb54V6mvteRhYWDovcjnFMsLfxRxVV",
       "1hJdgnAPSjfuHZFHzcorPnFvekSHihK9jdNPWHXgeuL7zaJ",
+      "13eKBARPFWBdXJAKg4fBTNUfcz4YAYfDTetRRApuz1kTDVDg",
+      "1zugcajKZ8XwjWvC5QZWcrpjfnjZZ9FfxRB9f5Hy6GdXBpZ",
+      "1zugcaiwmKdWsfuubmCMBgKKMLSef2TEC3Gfvv5GxLGTKMN",
     ]);
   }
 
@@ -63,6 +66,66 @@ async function init() {
       {
         address: "14z4r6EJMkCeQyxrAuHfdbVJRESA3veXDcTxcViHuMj5dEEA",
         ethereumAddress: "0xdeFF2Cd841Bd47592760cE068a113b8E594F8553"
+          .toLowerCase()
+          .split("x")[1],
+      },
+      {
+        address: "13mbLCRUQyFQWEHniQQLZhkpRbJGw4Dkttec9fUuaKkR95X5",
+        ethereumAddress: "0x60fEB537C1412d1C5ea5462a0984F058c347BC13"
+          .toLowerCase()
+          .split("x")[1],
+      },
+      {
+        address: "15oqs89aVy6bgk7PwyULibbxzyowGBjPxuQAduxeHor3pn9Y",
+        ethereumAddress: "0xd2D0cD3E49Ee6724d1d403E3D4a9ee7AA8aD7627"
+          .toLowerCase()
+          .split("x")[1],
+      },
+      {
+        address: "14oUkrAXTfDpYtwuhAt1twZXCQ5VFfqE3dyXXY8WTFyexnez",
+        ethereumAddress: "0x46D07d37704B15388d97Ca5F95E64F6d9801f9BF"
+          .toLowerCase()
+          .split("x")[1],
+      },
+      {
+        address: "1nfsMtBMV2b5CWr1PGgRhsPp7eg5wog6Aw7QF6rueWASqMU",
+        ethereumAddress: "0x90C1AB208Bc7C22a4306BC70899622934BF0F513"
+          .toLowerCase()
+          .split("x")[1],
+      },
+      {
+        address: "124abMcBcToayqnaC3XhZZTmEwXR82B7PbwxXw1PuriRLXff",
+        ethereumAddress: "0xFB22c0B729BF5F56aD904f71307FC247A82C2AF5"
+          .toLowerCase()
+          .split("x")[1],
+      },
+      {
+        address: "14GPSy2ugNGEV2ypB6X6h8yZ8sRiLEsyBHj6SemJPg36Q4P6",
+        ethereumAddress: "0x15A9Cdbf563a613d4A07c890aC7A404a17157236"
+          .toLowerCase()
+          .split("x")[1],
+      },
+      {
+        address: "138J63FK8DcZMRgfBXmvpMXFVztwwT3MA87pWrTW8YDLjLKZ",
+        ethereumAddress: "0x8da03780fcAF72e490c17e4cab8ad63195bE2084"
+          .toLowerCase()
+          .split("x")[1],
+      },
+      {
+        address: "15Dk12SYAncNj5vhjszjGetWhPAXrcShnC4tnh9Y3rbgUpis",
+        ethereumAddress: "0x2a63a4188082270f172Ff8988fBaB252e4201BEe"
+          .toLowerCase()
+          .split("x")[1],
+      },
+      {
+        address: "15Mo7G4HY7c9RvR4ooKS975SQxj8f2Htxxw7VT99BHg1A6ug",
+        ethereumAddress: "0x811e09Fd507730E9D42424d828908f714C5A607C"
+          .toLowerCase()
+          .split("x")[1],
+      },
+      {
+        address: "121fFdzRKVJEPAcK2jqY3iJFwuue1MsUCXbEgdqiV9BsBDsS",
+        ethereumAddress: "0xF1A1f124BA6914087f54825b4bCF1907B61d718E"
           .toLowerCase()
           .split("x")[1],
       },
